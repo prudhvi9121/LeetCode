caa29d73905c3ee625304e0db5b35418ee0e0042
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-01-09 00:00:35 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (10.30%) - LeetSync

--- 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
     int helper(int n){
-        cout<<n<<endl;
         if((n&(n-1))==0) return 1;
         int k=floor(log2(n));
-        int k2=ceil(log2(n));
-        return 1+helper(min(abs(n-(1<<k)), abs(n-(1<<k2)) ));
+        return 1+helper(min(abs(n-(1<<k)), abs(n-(1<<(k+1))) ));
     }
     int minOperations(int n) {
         return helper(n);
-        int cnt=0;
-        int k=floor(log2(n));
-        int k2=ceil(log2(n));
-        cout<<k<<" "<<k2<<endl;
-        // while(n){
-        //     int k=floor(log2(n));
-            
-        //     cout<<n<<" "<<(1<<k)<<endl;
-        //     n-=(1<<k);
-        //     cnt++;
-        // }
-        return cnt;
-    }
+            }
 };
