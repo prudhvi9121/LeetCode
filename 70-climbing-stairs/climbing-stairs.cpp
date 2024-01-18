56ac08fad331ba2aae884c3c7beb79b2f68fa591
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-01-18 10:23:57 +0530

    Time: 2 ms (45.07%) | Memory: 6.5 MB (59.54%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int>arr(n,0);
+        arr[0]=1;
+        if(n==1) return arr[n-1];
+        arr[1]=2;
+        for(int i=2;i<n;i++)
+        {
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n-1];
+    }
+};
