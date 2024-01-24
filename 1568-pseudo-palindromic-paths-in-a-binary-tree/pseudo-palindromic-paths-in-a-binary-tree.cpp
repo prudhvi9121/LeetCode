8fa57bd081fb97962b01549d406992e5809c83df
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-01-24 13:12:57 +0530

    Time: 232 ms (96.78%) | Memory: 177 MB (99.46%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -12,28 +12,25 @@
 class Solution {
 public:
     int res=0;
-    void palindrome(TreeNode* root,vector<int> vec){
+    void palindrome(TreeNode* root,int vec){
+        if( (vec>>(root->val)) & 1 ){
+            vec ^= (1<<root->val);
+        }
+        else{
+            vec |= (1<<root->val);
+        }
         if(!root)return ;
         if(!root->left and !root->right){
-            vec[root->val-1]+=1;
-            int k=0;
-            int path=0;
-            for(auto it:vec){
-                path+=it;
-               // cout<<it<<" ";
-                if(it&1)k++;
-            }
-          // cout<<" -"<<path<<" "<<k<<endl;
-            if(((path&1) and k==1) or ((path%2==0) and k==0)) res++;
+            int f = __builtin_popcount(vec);
+            if(f==0 or f==1) res+=1;
             return ;
         }
-        vec[root->val-1]+=1;
-        palindrome(root->left,vec);
-        palindrome(root->right,vec);
+        if(root->left)palindrome(root->left,vec);
+        if(root->right)palindrome(root->right,vec);
     }
     int pseudoPalindromicPaths (TreeNode* root) {
         vector<int>vec(9,0);
-        palindrome(root,vec);
+        palindrome(root,0);
         return res;
     }
 };
