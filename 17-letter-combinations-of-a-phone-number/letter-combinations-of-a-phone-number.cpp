9659827357060d2cbf7049a6a94fa4551d633df5
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-01-30 18:20:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (5.20%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    vector<string>res;
+
     vector<string>samp{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    void Phonecombos(string dig,int i,vector<string>ls){
-        if(i==dig.length()){
-            for(auto it:ls){
-                res.push_back(it);
-            }
+    void Phonecombos(string dig,int i,vector<string>&ls,string st){
+        if(i>=dig.length()){
+            ls.push_back(st);
             return ;
         }
         int digit=dig[i]-'0';
-        vector<string>examp(ls.begin(),ls.end());
-        // for(auto k:examp){
-        //     cout<<k<<endl;
-        // }
-       // cout<<samp[]
-        ls.erase(ls.begin(),ls.end());
         for(int k=0;k<samp[digit].size();k++){
-            for(auto it:examp){
-                ls.push_back(it+samp[digit][k]);
-            }
+            st.push_back(samp[digit][k]);
+            cout<<st<<endl;
+            Phonecombos(dig,i+1,ls,st);
+            st.pop_back();
         }
-        Phonecombos(dig,i+1,ls);
     }
     vector<string> letterCombinations(string digits) {
         if(digits=="") return {};
-        vector<string>ls{""};
-        Phonecombos(digits,0,ls);
-        
-        return res;
+        vector<string>ls;
+        Phonecombos(digits,0,ls,"");
+        return ls;
     }
 };
