3282492dea79498396de90318d8e029a6d64088b
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-02-16 10:35:39 +0530

    Time: 108 ms (91.08%) | Memory: 66 MB (60.95%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-  static bool cmp(const pair<int,int>& a, const pair<int,int>& b){
-    return (a.second < b.second);
-    }
+//   static bool cmp(const pair<int,int>& a, const pair<int,int>& b){
+//     return (a.second < b.second);
+//     }
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
         unordered_map<int,int> mp;
         for(auto it: arr)
             mp[it]++;
             
-        vector<pair<int,int>> mm;
+        vector<int> mm;
         for(auto it: mp){
-            mm.push_back(make_pair(it.first, it.second));
+            mm.push_back(it.second);
         }
-        sort(mm.begin(), mm.end(), cmp);
+        sort(mm.begin(), mm.end());
         int count = mm.size();
         for(auto it: mm) {
-            if(k >= it.second) {
-                k -= it.second;
+            if(k >= it) {
+                k -= it;
                 count--;
             } else {
                 break;
