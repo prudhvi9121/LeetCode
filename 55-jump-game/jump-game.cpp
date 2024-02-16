c91602cab5cad19a8375d4d6383be967b88588c0
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-02-16 18:52:44 +0530

    Time: 45 ms (69.77%) | Memory: 53 MB (7.34%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>vec(n,0);
+        if(n==1) return 1;
+        int flag=0;
+
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==0 && flag<=i) return 0;
+            else if(i+nums[i]<(n-1)){
+                vec[i+nums[i]]=1;
+                flag=max(flag,i+nums[i]);
+            }
+            else return 1;
+        }
+        cout<<"hi"<<endl;
+        if(vec[n-1]==1) return 1;
+        return 0;
+    }
+};
