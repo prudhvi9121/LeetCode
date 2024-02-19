2c859cc64027c91831c163fac7c97e70bc5608c0
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-02-19 22:43:55 +0530

    Time: 36 ms (74.00%) | Memory: 12.9 MB (88.44%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int dp[10005];
-    int coins_cc(vector<int>&coins,int amount){
+    int dp[10005][13];
+    int coins_cc(vector<int>&coins,int amount,int n){
         if(amount==0) return 0;
-        if(amount<0) return 1e9;
-        if(dp[amount]!=-1) return dp[amount];
+        if(n<0) return 1e9;
+        if(dp[amount][n]!=-1) return dp[amount][n];
         int pick=1e9; 
-        for(int i=coins.size()-1;i>=0;i--){
-            if(coins[i]<=amount){
-                pick=min(pick,1+coins_cc(coins,amount-coins[i]));
-            } 
-        }
-        return dp[amount]=pick;
+        if(coins[n]<=amount) pick=1+coins_cc(coins,amount-coins[n],n);
+        int nopick=0+coins_cc(coins,amount,n-1);
+        return dp[amount][n]=min(pick,nopick);
     }
     int coinChange(vector<int>& coins, int amount) {
-         sort(coins.begin(),coins.end());
+        sort(coins.begin(),coins.end());
         memset(dp,-1,sizeof(dp));
-        int k= coins_cc(coins,amount);
+        int k= coins_cc(coins,amount,coins.size()-1);
         if(k==1e9) return -1;
         return k;
     }
