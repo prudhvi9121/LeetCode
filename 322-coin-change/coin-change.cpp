fee3e3e79f8536e80aa76932b7bdbcb50c9e7547
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-02-19 22:41:02 +0530

    Time: 29 ms (79.37%) | Memory: 12.7 MB (88.84%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[100005];
+    int coins_cc(vector<int>&coins,int amount){
+        if(amount==0) return 0;
+        if(amount<0) return 1e9;
+        if(dp[amount]!=-1) return dp[amount];
+        int pick=1e9; 
+        for(int i=coins.size()-1;i>=0;i--){
+            if(coins[i]<=amount){
+                pick=min(pick,1+coins_cc(coins,amount-coins[i]));
+            } 
+        }
+        return dp[amount]=pick;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+         sort(coins.begin(),coins.end());
+        memset(dp,-1,sizeof(dp));
+        int k= coins_cc(coins,amount);
+        if(k==1e9) return -1;
+        return k;
+    }
+};
