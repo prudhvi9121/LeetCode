56ba6186638efbd904386c7c6efc8a04ae79c542
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-02-20 22:46:22 +0530

    Time: 14 ms (44.22%) | Memory: 18.5 MB (15.84%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    unordered_map<string,int>mp;
+    int dp[305];
+    int word(string s,int i){
+        if(i==s.size()) return 1;
+        if(dp[i]!=-1) return dp[i];
+        int k=0;
+        for(int j=i+1;j<=s.size();j++){
+            if(mp.find(s.substr(i,j-i))!=mp.end()){
+                k=word(s,j);
+                dp[i]=k;
+            }
+            if(k==1) return 1;
+        }
+        return k;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        memset(dp,-1,sizeof(dp));
+        for(auto it:wordDict) mp[it]++;
+        return word(s,0);
+    }
+};
