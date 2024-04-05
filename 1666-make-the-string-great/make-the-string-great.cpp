6d55f13b07dbe1f11cf4cbedd6adc2befa252df0
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-04-06 01:18:03 +0530

    Time: 4 ms (42.17%) | Memory: 7.6 MB (89.98%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string res;
+        for(int i=0;i<s.size();i++){
+            if (res!="" && abs(s[i] - res[res.size()-1]) == 32) {
+                res.pop_back();
+            } else {
+                res+=s[i];
+            }
+        }
+        return res;
+    }
+};
