bd56964339c5c12fd2a4717f55043a8adfea097e
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-04-27 07:57:31 +0530

    Time: 4 ms (67.54%) | Memory: 13.4 MB (40.66%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int cnt=1,res=0,n=nums.size();
-        for (int i = 0; i < n-1; i++) {
-            if(nums[i]==nums[i+1]){
+        int cnt = 1, res = 0, n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
                 cnt++;
-                if(cnt>2){
-                    nums[i]=100000;
+                if (cnt > 2) {
+                    nums[i] = 100000;
                     res++;
                     nums.push_back(-100000);
                 }
-            }
-            else{
-                cnt=1;
+            } else {
+                cnt = 1;
             }
         }
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]==100000){
-        //         res++;
-        //         nums.erase(nums.begin() + i);
-        //     }
-        // }
         nums.erase(remove(nums.begin(), nums.end(), 100000), nums.end());
-        for(auto it:nums) cout<<it<<" ";
-        return n-res;
+        return n - res;
     }
 };
