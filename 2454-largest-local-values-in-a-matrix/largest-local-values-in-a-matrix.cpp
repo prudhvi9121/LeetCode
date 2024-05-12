320b422052c3810e241887bf3adcc01c42398e52
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-05-12 16:50:57 +0530

    Time: 11 ms (53.93%) | Memory: 13.6 MB (53.31%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.cpp
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.cpp
@@ -5,11 +5,8 @@ public:
         vector<vector<int>>res(n-2,vector<int>(n-2,0));
         for (int i = 0; i < n - 2; i++){
             for(int  j = 0; j < n - 2; j++){
-                res[i][j]=max({ grid[i][j], grid[i][j + 1], grid[i][j + 2], 
-                                       grid[i + 1][j], grid[i + 1][j + 1], grid[i + 1][j + 2], 
-                                       grid[i + 2][j], grid[i + 2][j + 1], grid[i + 2][j + 2] });
-            }            }
-        
-        return res;
+                res[i][j]=max({ grid[i][j], grid[i][j + 1], grid[i][j + 2],grid[i + 1][j], grid[i + 1][j + 1], grid[i + 1][j + 2],grid[i + 2][j], grid[i + 2][j + 1], grid[i + 2][j + 2] });
+            }            
+        } return res;
     }
 };
