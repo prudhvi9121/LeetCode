3cbb92add1773fd7d16972d8ace3c994f97a41c5
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-05-13 21:22:28 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (75.07%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     int matrixScore(vector<vector<int>>& grid) {
-        int row=grid.size();
-        int col=grid[0].size();
-        for(int k=0;k<row;k++){
-            if(!grid[k][0]){
-                for(int i=0;i<col;i++) grid[k][i]=grid[k][i]^1;
+        int row = grid.size(),col = grid[0].size();
+        for (int k = 0; k < row; k++) {
+            if (!grid[k][0]) {
+                for (int i = 0; i < col; i++)
+                    grid[k][i] = grid[k][i] ^ 1;
             }
         }
-        for(int i=1;i<col;i++){
-            int c0=0,c1=0;
-            for(int j=0;j<row;j++){
-                if(grid[j][i]) c1++;
+        for (int i = 1; i < col; i++) {
+            int c0 = 0, c1 = 0;
+            for (int j = 0; j < row; j++) {
+                if (grid[j][i])
+                    c1++;
             }
-            c0=row-c1;
-            if(c0>c1){
-                for(int j=0;j<row;j++){
-                    grid[j][i]=grid[j][i]^1;
+            c0 = row - c1;
+            if (c0 > c1) {
+                for (int j = 0; j < row; j++) {
+                    grid[j][i] = grid[j][i] ^ 1;
                 }
             }
         }
-        int res=0;
-        for(int i=0;i<row;i++){
-            int bit=0;
-            for(int j=0;j<col;j++){
-                // cout<<grid[i][j]<<" ";
-                if(grid[i][j])bit+=(1<<(col-1-j));
+        int res = 0;
+        for (int i = 0; i < row; i++) {
+            int bit = 0;
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j])
+                    bit += (1 << (col - 1 - j));
             }
-            // cout<<endl;
-            res+=bit;
+            res += bit;
         }
         return res;
     }
