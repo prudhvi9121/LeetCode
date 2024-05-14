a9ba1fd552f4dde391d9cf5227767adfb01b1d90
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-05-14 22:09:43 +0530

    Time: 1295 ms (5.13%) | Memory: 252.7 MB (5.13%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxGold;
-    void dfs(vector<vector<int>>& grid, int i, int j, vector<vector<bool>>& visited, int currentGold) {
+    void dfs(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& visited, int currentGold) {
         if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0 || visited[i][j]) {
             maxGold = max(maxGold, currentGold);
             return;
         }
-        visited[i][j] = true;
+        visited[i][j] = 1;
         currentGold += grid[i][j];
         vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         for (auto& dir : directions) {
@@ -14,13 +14,13 @@ public:
             int nj = j + dir.second;
             dfs(grid, ni, nj, visited, currentGold);
         }
-        visited[i][j] = false; 
+        visited[i][j] = 0; 
     }
     int getMaximumGold(vector<vector<int>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
         maxGold = 0;
-        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+        vector<vector<int>> visited(rows, vector<int>(cols, 0));
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
                 if (grid[i][j] != 0) {
