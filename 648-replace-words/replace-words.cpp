98589ad6d4d6b338165428c3d3219456ef504eeb
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-06-07 22:02:55 +0530

    Time: 207 ms (33.02%) | Memory: 44.5 MB (86.74%) - LeetSync

---
+++ 648-replace-words/replace-words.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dic, string sentence) {
+        unordered_map<string, int> mp;
+        for (auto it : dic)
+            mp[it]++;
+        string res,temp;
+        int flg=0;
+        for (int i = 0; i < sentence.size(); i++) {
+            if(sentence[i]==' '){
+                res+=temp+" ";
+                temp="";
+                flg=0;
+            }
+            else{
+                if(!flg){
+                    temp+=sentence[i];
+                }
+                if(mp.find(temp)!=mp.end()){
+                    flg=1;
+                }
+            }
+        }
+        res+=temp;
+        return res;
+    }
+};
