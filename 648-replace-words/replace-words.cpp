6ebb1e7a58b0943e2ad2ef52f31959a44c213427
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-06-07 22:03:11 +0530

    Time: 205 ms (33.49%) | Memory: 44.5 MB (86.74%) - LeetSync

--- 648-replace-words/replace-words.cpp
+++ 648-replace-words/replace-words.cpp
@@ -4,24 +4,23 @@ public:
         unordered_map<string, int> mp;
         for (auto it : dic)
             mp[it]++;
-        string res,temp;
-        int flg=0;
+        string res, temp;
+        int flg = 0;
         for (int i = 0; i < sentence.size(); i++) {
-            if(sentence[i]==' '){
-                res+=temp+" ";
-                temp="";
-                flg=0;
-            }
-            else{
-                if(!flg){
-                    temp+=sentence[i];
+            if (sentence[i] == ' ') {
+                res += temp + " ";
+                temp = "";
+                flg = 0;
+            } else {
+                if (!flg) {
+                    temp += sentence[i];
                 }
-                if(mp.find(temp)!=mp.end()){
-                    flg=1;
+                if (mp.find(temp) != mp.end()) {
+                    flg = 1;
                 }
             }
         }
-        res+=temp;
+        res += temp;
         return res;
     }
 };
