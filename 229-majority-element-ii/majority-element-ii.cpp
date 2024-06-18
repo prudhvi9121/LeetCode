818ef901bccda1891741020c560dcdd22702133c
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-06-18 19:00:15 +0530

    Time: 8 ms (65.54%) | Memory: 18.4 MB (70.98%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int el1=INT_MIN,el2=INT_MIN,cnt1=0,cnt2=0;
-        for(int i=0;i<nums.size();i++){
-            if(cnt1==0 && el2!=nums[i]){
-                cnt1=1;
-                el1=nums[i];
-            }else if(cnt2==0 && el1!=nums[i]){
-                cnt2=1;
-                el2=nums[i];
-            }else if(el1==nums[i]){
+        int el1 = INT_MIN, el2 = INT_MIN, cnt1 = 0, cnt2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (cnt1 == 0 && el2 != nums[i]) {
+                cnt1 = 1;
+                el1 = nums[i];
+            } else if (cnt2 == 0 && el1 != nums[i]) {
+                cnt2 = 1;
+                el2 = nums[i];
+            } else if (el1 == nums[i]) {
                 cnt1++;
-            }else if(el2==nums[i]){
+            } else if (el2 == nums[i]) {
                 cnt2++;
-            }else{
+            } else {
                 cnt1--;
                 cnt2--;
             }
-
         }
         cnt1 = 0, cnt2 = 0;
-        for(auto it: nums) {
-            if(el1 == it) cnt1++;
-            if(el2 == it) cnt2++;
+        for (auto it : nums) {
+            if (el1 == it)
+                cnt1++;
+            if (el2 == it)
+                cnt2++;
         }
-        vector<int>ans;
-        int mini = (nums.size()/3) +1;
-        if(cnt1 >= mini) ans.push_back(el1);
-        if(cnt2 >= mini) ans.push_back(el2);
+        vector<int> ans;
+        int mini = (nums.size() / 3) + 1;
+        if (cnt1 >= mini)
+            ans.push_back(el1);
+        if (cnt2 >= mini)
+            ans.push_back(el2);
         return ans;
     }
 };
