90d0f2c629ad364bdce03600e28d84226da11513
prudhvi9121 <96787579+prudhvi9121@users.noreply.github.com>
2024-06-21 15:52:29 +0530

    Time: 4 ms (57.04%) | Memory: 14.7 MB (24.72%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -14,25 +14,18 @@ class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         map<int, map<int, multiset<int>>> mp;
-        queue<pair<TreeNode*, pair<int,int>>> qq;
-        qq.push({root, {0,0}});
+        queue<pair<TreeNode*, pair<int, int>>> qq;
+        qq.push({root, {0, 0}});
         while (!qq.empty()) {
             auto it = qq.front();
             qq.pop();
             TreeNode* nn = it.first;
-            int lev = it.second.first,hlev=it.second.second;
+            int lev = it.second.first, hlev = it.second.second;
             mp[lev][hlev].insert(nn->val);
-            // mp[lev].push_back(nn->val);
-            // for(auto p:mp){
-            //     cout<<p.first<<": ";
-            //     for(auto op:p.second) cout<<op<<" ";
-            //     cout<<endl;
-            // }
-            // cout<<"-------"<<endl;
             if (nn->left)
-                qq.push({nn->left, {lev - 1,hlev+1}});
+                qq.push({nn->left, {lev - 1, hlev + 1}});
             if (nn->right)
-                qq.push({nn->right, {lev + 1,hlev+1}});
+                qq.push({nn->right, {lev + 1, hlev + 1}});
         }
         vector<vector<int>> res;
         for (auto p : mp) {
@@ -42,7 +35,7 @@ public:
             }
             res.push_back(col);
         }
-        
+
         return res;
     }
 };
