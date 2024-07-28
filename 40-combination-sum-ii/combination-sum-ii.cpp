f03693bdc22fd6850e1c4a058a83e56128607c6e
Prudhvi Karri <96787579+prudhvi9121@users.noreply.github.com>
2024-07-28 20:46:38 +0530

    Time: 3 ms (78.26%) | Memory: 14.2 MB (21.24%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    set<vector<int>> res;
-    void combo(int i,vector<int>&cand,int target,vector<int>& samp){
-        if(target==0){
-           // sort(samp.begin(),samp.end());
-            res.insert(samp);
+    set<vector<int>>ans;
+    void solve(vector<int>&samp,vector<int>&cand,int tar,int i){
+        if(tar==0){
+            ans.insert(samp);
             return ;
         }
-         if(target<0 or i>=cand.size()) return ;
-        if(cand[i]<=target){
-            samp.push_back(cand[i]);
-            combo(i+1,cand,target-cand[i],samp);
-            samp.pop_back();
-            //combo(i+1,cand,target,samp);
-        }
+        if(i>=cand.size() || tar<=0) return ;
+        samp.push_back(cand[i]);
+        solve(samp,cand,tar-cand[i],i+1);
+        samp.pop_back();
         int j=i+1;
         while(j<cand.size() && cand[j]==cand[j-1]) j++;
-        combo(j,cand,target,samp);
-       // samp.pop_back();
+        solve(samp,cand,tar,j);
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<int>samp;
-       // return {{}};
         sort(candidates.begin(),candidates.end());
-        combo(0,candidates,target,samp);
-        vector<vector<int>>res1(res.begin(),res.end());
-        return res1;
+        vector<int>samp;
+        solve(samp,candidates,target,0);
+        return vector(ans.begin(),ans.end());
     }
 };
