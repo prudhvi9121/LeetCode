710a0b6908cead4e3de11c407607a3a8dba101d2
Prudhvi Karri <96787579+prudhvi9121@users.noreply.github.com>
2024-10-08 21:27:51 +0530

    Time: 3 ms (65.67%) | Memory: 12.3 MB (41.17%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool binarysearch(int low,int high,int row,vector<vector<int>>&matrix,int tar){
+        cout<<"HEllo";
+        while(low<=high){
+            int mid=(high+low)/2;
+            if(matrix[row][mid]==tar) return true;
+            else if(matrix[row][mid]<tar) low=mid+1;
+            else high=mid-1;
+        }return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+        for(int i=0;i<row;i++){
+            if(matrix[i][0]<= target && matrix[i][col-1]>=target && binarysearch(0,col-1,i,matrix,target))
+            return true;
+        }
+        return false;
+    }
+};
