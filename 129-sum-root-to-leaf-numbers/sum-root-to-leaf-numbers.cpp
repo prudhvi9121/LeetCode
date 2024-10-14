7fce56004ced5760ff2dc4145f03bff22a2508db
Prudhvi Karri <96787579+prudhvi9121@users.noreply.github.com>
2024-10-14 21:50:02 +0530

    Time: 2 ms (45.15%) | Memory: 11.3 MB (63.86%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -15,9 +15,7 @@ public:
         if(!root) return 0;
         sum = (sum*10) + root->val;
         if(!root->left && !root->right) return sum;
-        int left = find_sum(root->left,sum);
-        int right = find_sum(root->right,sum);
-        return left + right;
+        return find_sum(root->left,sum) + find_sum(root->right,sum);        
     }
     int sumNumbers(TreeNode* root) {
         return find_sum(root,0);
